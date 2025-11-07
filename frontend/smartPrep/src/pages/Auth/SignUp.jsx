7834038c5e7fc46f40872198a13ec79b059fceb6
aryandumale04 <aryandumale04@gmail.com>
2025-11-07 14:43:42 +0530

    Add Sign UP form

--- frontend/smartPrep/src/pages/Auth/SignUp.jsx
+++ frontend/smartPrep/src/pages/Auth/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input"
 
 const SignUp = ({setCurrentPage}) => {
   const [profilePic, setProfilePic] = useState(null);
@@ -10,8 +11,61 @@ const SignUp = ({setCurrentPage}) => {
   const[error, setError] = useState(null);
 
   const navigate = useNavigate();
+
+  //Handle SignUp Form Submit
+  const handleSignUp = async (e) =>{
+    e.preventDefault();
+  };
   return (
-    <div>SignUp</div>
+    <div className='w-[90vw] md:w-[33vw] p-7 flex flex-col justify-center'>
+      <h3 className=''>
+        Create an Account
+      </h3>
+      <p className=''>
+        Join us today by entering your details below.
+      </p>
+      <form onSubmit={handleSignUp}>
+        <div className=''>
+          <Input value={fullName}
+          onChange={({ target }) => setFullName(target.value)}
+          label="Full Name"
+          placeholder="John"
+          type="text">
+          </Input>
+          <Input value={email}
+          onChange={({ target })=> setEmail(target.value)}
+          label="Email Address"
+          placeholder="john@example.com"
+          type="text"
+          >
+          </Input>
+          <Input value={password}
+          onChange={({ target })=> setPassword(target.value)}
+          label="Password"
+          placeholder="Minimum 8 Characters"
+          type="password"
+          >
+          </Input>
+        </div>
+        {error && <p className=''>{error}</p>}
+
+        <button type='submit' className=''>
+          SIGN UP
+
+        </button>
+        <p className=''>
+          Already have an account?{" "}
+          <button className=''
+          onClick={() => {
+            setCurrentPage("login");
+          }}>
+            Login
+
+          </button>
+        </p>
+      </form>
+
+    </div>
   )
 }
 
