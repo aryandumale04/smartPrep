fdadca6e15d675a58b9edf99387a8e64c4a52061
aryandumale04 <aryandumale04@gmail.com>
2025-11-20 18:01:54 +0530

    Set up Auth Routes, Middleware and Controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const path = require("path")
 const  connectDB =  require("../backend/config/db");
 const app = express();
 
+const authRoutes = require("./routes/authRoutes");
+// const sessionRoutes = require("./routes/sessionRoutes");
+// const questionRoutes = require("./routes/questionRoutes"); 
+const { protect } = require("./middlewares/authMiddleware");
 //Middleware to handle CORS
 app.use(
     cors({
@@ -20,6 +24,13 @@ app.use(express.json());
 
 
 //Routes
+app.use("/api/auth", authRoutes);
+// app.use("/api/sessions", sessionRoutes);
+// app.use("/api/questions",questionRoutes);
+
+// app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
+// app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+
 
 //Server uploads folder 
 app.use("/uploads",express.static(path.join(__dirname,"uploads"), {}));
