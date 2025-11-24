ded81e981a30e29b68f206d03c5e6160f8bb14b6
aryandumale04 <aryandumale04@gmail.com>
2025-11-24 12:28:18 +0530

    Add Question API's

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
-// const questionRoutes = require("./routes/questionRoutes"); 
+const questionRoutes = require("./routes/questionRoutes"); 
 const { protect } = require("./middlewares/authMiddleware");
 
 
@@ -28,7 +28,7 @@ app.use(express.json());
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
-// app.use("/api/questions",questionRoutes);
+app.use("/api/questions",questionRoutes);
 
 // app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 // app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
